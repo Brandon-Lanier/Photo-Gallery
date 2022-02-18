4bc04437c7647c776b4382bdf91c0a2c31f4f05f
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-17 21:17:34 -0600

    Setting up for base goals

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+import GalleryList from '../GalleryList/GalleryList';
 import './App.css';
 
 function App() {
+
+const [gallery, setGallery] = useState([])
+
+useEffect(() => {
+  fetchGallery();
+  }, [])
+
+  const fetchGallery = () => {
+    axios.get('/gallery')
+        .then(response => {
+            setGallery(response.data);
+            console.log(gallery);
+        }).catch(error => {
+            console.log('Error fetching gallery');
+        })
+
+
+ }
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList 
+            gallery={gallery}
+        />
       </div>
     );
 }
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,14 @@
+
+
+function GalleryList({gallery}) {
+
+    return (
+        <div>
+            {gallery.map(picture =>)}
+
+        </div>
+
+    )
+}
+
+export default GalleryList;
