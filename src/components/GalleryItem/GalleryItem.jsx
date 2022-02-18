d2908fedadc7c4331127735ad1eaca923ae153f1
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-17 22:25:40 -0600

    Item is rending to DOM

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import './GalleryItem.css'
 
 function GalleryItem({item, updateLikes}) {
+
+    const [description, setDescription] = useState(false)
 
     const handleLike = () => {
         updateLikes(item.id);
     }
 
+    const handleClick = () => {
+        setDescription(true);
+    }
+
     return (
-        <div className="picBox">
+        <div className="galleryBox">
+            <div className="picBox" onClick={handleClick}>
             <img src={item.path} alt="Gallery Photo" className="galleryPicture" />
-            {item.description}
+            <p>{item.description}</p>
+
+            </div>
             <button onClick={handleLike}>Like</button>
             <p>Total Likes {item.likes}</p>
 
